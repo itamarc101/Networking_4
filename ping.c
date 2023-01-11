1ad33b93eb180edb40c19385ff1423aeb897cf8c
itamarc101 <45041126+itamarc101@users.noreply.github.com>
2023-01-11 13:10:18 +0200

    Update ping.c

--- ping.c
+++ ping.c
@@ -1,217 +1,170 @@
-/*****************************************************************************/
-/*** myping.c                                                              ***/
-/***                                                                       ***/
-/*** Use the ICMP protocol to request "echo" from destination.             ***/
-/*****************************************************************************/
+// icmp.cpp
+// Robert Iakobashvili for Ariel uni, license BSD/MIT/Apache
+//
+// Sending ICMP Echo Requests using Raw-sockets.
+//
+#include <signal.h>
 #include <fcntl.h>
-#include <errno.h>
-#include <sys/socket.h>
-#include <resolv.h>
 #include <netdb.h>
-#include <netinet/in.h>
-#include <netinet/ip_icmp.h>
-#include <netinet/ip.h>
 #include <arpa/inet.h>
-#include <stdlib.h>
-#include <strings.h>
-#include <unistd.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/time.h> // gettimeofday()
 #include <sys/types.h>
-#include <sys/wait.h>
-#include <time.h>
-#define SOURCE_IP "10.0.2.15"
-// i.e the gateway or ping to google.com for their ip-address
-#define DESTINATION_IP "8.8.8.8"
+#include <unistd.h>
+#include <resolv.h>
+// IPv4 header len without options
+#define IP4_HDRLEN 20
+// ICMP header len for echo req
+#define ICMP_HDRLEN 8
+int timeCount = 0;
+int pacrec = 0;  // packet recived
+int pacsend = 0; // packet sends
+float mintime = 99999999; // min time
+float maxtime = 0; // max time
+float total = 0; // total time
+// Checksum algo
+unsigned short calculate_checksum(unsigned short *paddress, int len);
 
-#define PACKETSIZE 128
-struct packet
+void finish(int);
+
+void finish(int sig)
 {
-	struct icmphdr hdr;
-	char msg[PACKETSIZE - sizeof(struct icmphdr)];
-};
 
-int pid = -1;
-int loops = 25;
-struct protoent *proto = NULL;
-int data_bytes;
-
-
-/*--------------------------------------------------------------------*/
-/*--- checksum - standard 1s complement checksum                   ---*/
-/*--- Calculate checksum for ICMP packet (header and data)         ---*/
-/*--------------------------------------------------------------------*/
-unsigned short checksum(void *b, int len)
-{
-	unsigned short *buf = b;
-	unsigned int sum = 0;
-	unsigned short result;
-
-	for (sum = 0; len > 1; len -= 2)
-		sum += *buf++;
-	if (len == 1)
-		sum += *(unsigned char *)buf;
-	sum = (sum >> 16) + (sum & 0xFFFF);
-	sum += (sum >> 16);
-	result = ~sum;
-	return result;
+    signal(sig, SIG_IGN); 
+    float avr = total / (float)timeCount;//avarage time
+    int pacloss = 100-((pacrec*100)/pacsend); // loos packet
+    printf("\n");
+    printf("%d packets transmitted, %d received, %d%c packet loss, time %fms\n"
+    , pacsend, pacrec, pacloss, '%', total);
+    printf("rtt min / avg / max  = %.3f / %.3f / %.3f ms\n", mintime, avr, maxtime);
+    exit(0);
 }
 
-/*--------------------------------------------------------------------*/
-/*--- display - present echo info                                  ---*/
-/*--------------------------------------------------------------------*/
-void display(void *buf, int bytes)
+// Compute checksum (RFC 1071).
+unsigned short calculate_checksum(unsigned short *paddress, int len)
 {
-	int i;
-	struct iphdr *ip = buf;
-	struct icmphdr *icmp = buf + ip->ihl * 4;
-	int flag = 1;
-	struct timeval start, end;
-    //gettimeofday(&start, 0);
-	clock_t startt = clock();
-	
-	//printf("----------------\n");
-	char sourceIPAddrReadable[32] = {'\0'};
-	inet_ntop(AF_INET, &ip->saddr, sourceIPAddrReadable, sizeof(sourceIPAddrReadable));
-	char destinationIPAddrReadable[32] = {'\0'};
-	inet_ntop(AF_INET, &ip->daddr, destinationIPAddrReadable, sizeof(destinationIPAddrReadable));
-	clock_t endt = clock();
-	//gettimeofday(&end, 0);
-	float milliseconds = (endt - startt) * 1000.0f / CLOCKS_PER_SEC; 
-	// inet_ntoa(ip->saddr)
-	// inet_ntoa(ip->daddr)
-	// Print packet IP, seq number, time
-    printf("%d bytes from %s: icmp_seq=%d ttl=%d time=%.3f ms\n", bytes, sourceIPAddrReadable,
-	 icmp->un.echo.sequence,ip->ttl, milliseconds);
-	//printf("IPv%d: hdr-size=%d pkt-size=%d protocol=%d TTL=%d src=%s ",
-	//	   ip->version, ip->ihl * 4, ntohs(ip->tot_len), ip->protocol,
-	//	   ip->ttl, sourceIPAddrReadable);
-	//printf("dst=%s\n", destinationIPAddrReadable);
-	//if (icmp->un.echo.id == pid)
-	//{
-	//	printf("ICMP: type[%d/%d] checksum[%d] id[%d] seq[%d]\n",
-	//		   icmp->type, icmp->code, ntohs(icmp->checksum),
-	//		   icmp->un.echo.id, icmp->un.echo.sequence);
-	//}
+    int nleft = len;
+    int sum = 0;
+    unsigned short *w = paddress;
+    unsigned short answer = 0;
+
+    while (nleft > 1)
+    {
+        sum += *w++;
+        nleft -= 2;
+    }
+
+    if (nleft == 1)
+    {
+        *((unsigned char *)&answer) = *((unsigned char *)w);
+        sum += answer;
+    }
+
+    // add back carry outs from top 16 bits to low 16 bits
+    sum = (sum >> 16) + (sum & 0xffff); // add hi 16 to low 16
+    sum += (sum >> 16);                 // add carry
+    answer = ~sum;                      // truncate to 16 bits
+
+    return answer;
 }
 
-/*--------------------------------------------------------------------*/
-/*--- listener - separate process to listen for and collect messages--*/
-/*--------------------------------------------------------------------*/
-void listener(void)
+int header(char packet[IP_MAXPACKET], struct icmp *icmphdr) // create header
 {
-	int sd;
-	struct sockaddr_in addr;
-	unsigned char buf[1024];
+    // char packetdata[IP_MAXPACKET] = *packet;
+    char data[IP_MAXPACKET] = "This is the ping.\n";
+    int datalen = strlen(data) + 1;
+    (*icmphdr).icmp_type = ICMP_ECHO;
+    (*icmphdr).icmp_code = 0;
+    (*icmphdr).icmp_seq += 1;
+    (*icmphdr).icmp_id = 18;
+    (*icmphdr).icmp_cksum = 0;
+    memcpy((packet), icmphdr, ICMP_HDRLEN);
+    memcpy(packet + ICMP_HDRLEN, data, datalen);
+    (*icmphdr).icmp_cksum = calculate_checksum((unsigned short *)(packet), ICMP_HDRLEN + datalen);
+    memcpy((packet), icmphdr, ICMP_HDRLEN);
 
-	sd = socket(PF_INET, SOCK_RAW, proto->p_proto);
-	if (sd < 0)
-	{
-		perror("socket");
-		exit(0);
-	}
-	for (;;)
-	{
-		int bytes, len = sizeof(addr);
-
-		bzero(buf, sizeof(buf));
-		bytes = recvfrom(sd, buf, sizeof(buf), 0, (struct sockaddr *)&addr, &len);
-		if (bytes > 0){
-			display(buf, bytes);
-		}
-		else
-			perror("recvfrom");
-	}
-	exit(0);
+    return datalen;
 }
 
-/*--------------------------------------------------------------------*/
-/*--- ping - Create message and send it.                           ---*/
-/*--------------------------------------------------------------------*/
-void ping(struct sockaddr_in *addr)
+void ping(int sock, struct sockaddr_in *dest_in, char packet[IP_MAXPACKET], int datalen)//ping send
 {
-	const int val = 255;
-	int i, j, sd = 1;
-	int cnt = 0;
-	struct packet pckt;
-	struct sockaddr_in r_addr;
 
-
-	sd = socket(PF_INET, SOCK_RAW, proto->p_proto);
-	if (sd < 0)
-	{
-		perror("socket");
-		return;
-	}
-	if (setsockopt(sd, SOL_IP, IP_TTL, &val, sizeof(val)) != 0)
-		perror("Set TTL option");
-	if (fcntl(sd, F_SETFL, O_NONBLOCK) != 0)
-		perror("Request nonblocking I/O");
-	
-	for (j = 0; j < loops; j++) // send pings 1 per second
-	{
-		int len = sizeof(r_addr);
-		//printf("Msg #%d\n", cnt);
-		if (recvfrom(sd, &pckt, sizeof(pckt), 0, (struct sockaddr *)&r_addr, &len) < 0)
-			printf("Msg");
-			
-		bzero(&pckt, sizeof(pckt));
-		pckt.hdr.type = ICMP_ECHO;
-		pckt.hdr.un.echo.id = pid;
-		for (i = 0; i < sizeof(pckt.msg) - 1; i++)
-			pckt.msg[i] = i + '0';
-		pckt.msg[i] = 0;
-		pckt.hdr.un.echo.sequence = cnt++;
-		pckt.hdr.checksum = checksum(&pckt, sizeof(pckt));
-		if (sendto(sd, &pckt, sizeof(pckt), 0, (struct sockaddr *)addr, sizeof(*addr)) <= 0)
-			perror("sendto");
-		sleep(1);
-	}
+    int bytes_sent = sendto(sock, packet, ICMP_HDRLEN + datalen, 0, (struct sockaddr *)dest_in, sizeof((*dest_in)));
+    pacsend++; // inc packet sent count
+    if (bytes_sent == -1)
+    {
+        fprintf(stderr, "Send packet failes, eror: %d", errno);
+        exit(0);
+    }
 }
+ssize_t listener(int sock, struct sockaddr_in *dest_in, char packet[IP_MAXPACKET])//recive ping
+{
+    bzero(packet, IP_MAXPACKET);
+    socklen_t len = sizeof(*dest_in);
+    ssize_t bytes_received = -1;
+    while ((bytes_received = recvfrom(sock, packet, IP_MAXPACKET, 0, (struct sockaddr *)dest_in, &len)))
+    {
+        if (bytes_received > 0)
+        {
 
-/*--------------------------------------------------------------------*/
-/*--- main - look up host and start ping processes.                ---*/
-/*--------------------------------------------------------------------*/
+            pacrec++; // inc ping recived count
+            return bytes_received;
+        }
+    }
+    return 0;
+}
 int main(int count, char *argv[])
 {
-	struct hostent *hname;
-	struct sockaddr_in addr;
-	//loops = 0;
-	if (count != 2)
-	{
-		printf("usage: %s <addr> \n", argv[0]);
-		exit(0);
-	}
-	if (count == 2) // WE HAVE SPECIFIED A MESSAGE COUNT
-		//loops = atoi(argv[1]);
-		loops = 25;
-
-	int icmp_header_size = sizeof(struct icmp);
-	
-	printf("PING %s (%s): %d data bytes\n", argv[1], argv[1], icmp_header_size);
-	if (count > 1)
-	{
-		
-		pid = getpid();
-		// printf("id main process = %d\n", pid);
-		proto = getprotobyname("ICMP");
-		hname = gethostbyname(argv[1]);
-		bzero(&addr, sizeof(addr));
-		addr.sin_family = hname->h_addrtype;
-		addr.sin_port = 0;
-		addr.sin_addr.s_addr = *(long *)hname->h_addr;
-		if (fork() == 0) /* child process */
-		{
-			// printf("child process with pid  = %d\n", (int) getpid());
-			listener();
-		}
-		else /* parent process */
-		{
-			// printf("parent process with pid  = %d\n", (int) getpid());
-			ping(&addr);
-		}
-		wait(0);
-	}
-	else
-		printf("usage: sudo ./parta <hostname>\n");
-	return 0;
+    if (count != 2) // set that command entered well
+    {
+        printf("usage: sudo ./parta <hostname>\n");
+        exit(0);
+    }
+    int datalen = 0;
+    struct icmp icmphdr; // ICMP-header
+    icmphdr.icmp_seq = 0;
+    struct sockaddr_in dest_in;
+    memset(&dest_in, 0, sizeof(struct sockaddr_in));
+    dest_in.sin_family = AF_INET;
+    dest_in.sin_addr.s_addr = inet_addr(argv[1]);
+    int sock = -1;
+    if ((sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) == -1) // create ping socket
+    {
+        fprintf(stderr, "Failed to creat socket: %d", errno);
+        return -1;
+    }
+    signal(SIGINT, finish); // catch ctrl + c
+    while (1)
+    {
+        char packet[IP_MAXPACKET] = {}; // packet buffer
+        bzero(packet, IP_MAXPACKET);
+        datalen = header(packet, &icmphdr); // create header
+        struct timeval start, end;
+        gettimeofday(&start, 0); // set start time
+        ping(sock, &dest_in, packet, datalen); // send ping
+        int bytes_received = listener(sock, &dest_in, packet); // recive ping
+        struct iphdr *iphdr = (struct iphdr *)packet;
+        struct icmp *icmpheader = (struct icmp *)(packet + (iphdr->ihl * 4));
+        gettimeofday(&end, 0); // set end time
+        char reply[IP_MAXPACKET];
+        memcpy(reply, packet + ICMP_HDRLEN + IP4_HDRLEN, datalen);
+        float milliseconds = (end.tv_sec - start.tv_sec) * 1000.0f + (end.tv_usec - start.tv_usec) / 1000.0f;
+        printf("%d bytes from %s: icmp_seq=%d ttl=%d time=%.3f ms\n", bytes_received, argv[1],
+               (*icmpheader).icmp_seq, iphdr->ttl, milliseconds); // print setting ping packet
+        timeCount++;
+        total += milliseconds;
+        mintime = MIN(mintime, milliseconds);
+        maxtime = MAX(maxtime, milliseconds);
+        sleep(1);
+    }
+    // Close the raw socket descriptor.
+    close(sock);
+    return 0;
 }
